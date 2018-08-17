e7325b2d3e39d410d81273ed1a5907ca6ecfb13e
Jan Grzegorowski <grzegorowski@gmail.com>
2018-08-17 09:31:56 +0200

    feat: add DrawerRight panel

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,17 @@
 // @flow
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
 import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Tooltip, Drawer, IconButton } from '@material-ui/core';
+import { HelpOutline as DrawerOpenIcon, Close as DrawerCloseIcon } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 import { Route } from 'react-router-dom';
-import PageHome from '../PageHome';
 
+import PageHome from '../PageHome';
+import DrawerRight from '../DrawerRight';
+
+// TODO: remember open/close state in local storage
+
+const drawerWidth = 360;
 const mediaRule = '@media (min-width:0px) and (orientation: landscape)';
 const styles = theme => ({
   root: {
@@ -44,54 +51,139 @@ const styles = theme => ({
     [theme.breakpoints.up('sm')]: {
       paddingTop: theme.mixins.toolbar[theme.breakpoints.up('sm')].minHeight,
     },
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  drawerTitle: {
+    paddingLeft: theme.spacing.unit * 2,
+  },
+  appBarShiftLeft: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  disabledBtn: {
+    color: `${theme.palette.common.white} !important`, // TODO: is it the only way to override default color?
   },
 });
 
 type Props = {
   classes: any;
 };
-const App = (props: Props) => {
-  const { classes } = props;
-  // let updateReady = false;
+type State = {
+  isOpenDrawer: boolean;
+};
+class App extends React.Component<Props, State> {
+  state = {
+    isOpenDrawer: true,
+  };
 
-  // let ipcRenderer;
-  // let installUpdates;
-  /*if (process.env.REACT_APP_ELECTRON) {
-    ipcRenderer = require('electron').ipcRenderer;
+  render = () => {
+    const { classes } = this.props;
+    const { isOpenDrawer } = this.state;
+    // let updateReady = false;
 
-    ipcRenderer.on('updateReady', function(event, text) {
-      updateReady = true;
-    });
+    // let ipcRenderer;
+    // let installUpdates;
+    /*if (process.env.REACT_APP_ELECTRON) {
+      ipcRenderer = require('electron').ipcRenderer;
 
-    installUpdates = (event) => {
-      ipcRenderer.send('quitAndInstall');
+      ipcRenderer.on('updateReady', function(event, text) {
+        updateReady = true;
+      });
+
+      installUpdates = (event) => {
+        ipcRenderer.send('quitAndInstall');
+      };
+    }*/
+    const toggleDrawer = (open: boolean) => (event: Event) => {
+      this.setState({ isOpenDrawer: open });
     };
-  }*/
 
-  return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar}>
-        <Toolbar disableGutters className={classes.toolbar}>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Influx Admin
-          </Typography>
-          <Typography variant="caption" color="inherit" className={classes.rightPanel}>
-            ver. <span id="version">{process.env.REACT_APP_VERSION}</span>
-            {/*process.env.REACT_APP_ELECTRON && updateReady &&
-              <Button onClick={installUpdates}>
-              Update
-              </Button>
-            */}
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    return (
+      <div className={classes.root}>
+        <AppBar className={classNames(classes.appBar, {
+          [classes.appBarShiftLeft]: isOpenDrawer,
+        })}>
+          <Toolbar disableGutters className={classes.toolbar}>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Influx Admin
+            </Typography>
+            <div className={classes.rightPanel}>
+              <Tooltip title="Additional info">
+                <div>
+                  <Button classes={{ disabled: classes.disabledBtn }} size="small" color="inherit" onClick={toggleDrawer(true)} disabled={isOpenDrawer}>
+                    <DrawerOpenIcon />
+                  </Button>
+                </div>
+              </Tooltip>
+            </div>
+            {/*<Typography variant="caption" color="inherit" className={classes.rightPanel}>
+              ver. <span id="version">{process.env.REACT_APP_VERSION}</span>
+              {/*process.env.REACT_APP_ELECTRON && updateReady &&
+                <Button onClick={installUpdates}>
+                Update
+                </Button>
+              }
+            </Typography>*/}
+          </Toolbar>
+        </AppBar>
 
-      <main className={classes.content}>
-        <Route path="/" component={PageHome} />
-      </main>
+        <main className={classNames(classes.content, {
+          [classes.contentShift]: isOpenDrawer,
+        })}>
+          <Route path="/" component={PageHome} />
+        </main>
 
-    </div>
-  );
+        <Drawer
+          variant="persistent"
+          anchor="right"
+          open={isOpenDrawer}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <Typography variant="subheading" color="inherit" className={classNames(classes.flex, classes.drawerTitle)}>
+              Additional info
+            </Typography>
+            <IconButton onClick={toggleDrawer(false)}>
+              <DrawerCloseIcon />
+            </IconButton>
+          </div>
+          <DrawerRight/>
+        </Drawer>
+
+      </div>
+    );
+  }
 };
 
 export default withStyles(styles, { withTheme: true })(App);
---
+++ src/components/DrawerRight/index.jsx
@@ -0,0 +1,52 @@
+// @flow
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
+
+const DrawerRight = ({ classes }) => (
+  <div className={classes.root}>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Expansion Panel 1</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+          sit amet blandit leo lobortis eget.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Expansion Panel 2</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+          sit amet blandit leo lobortis eget.
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+    <ExpansionPanel disabled>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.heading}>Disabled Expansion Panel</Typography>
+      </ExpansionPanelSummary>
+    </ExpansionPanel>
+  </div>
+);
+
+export default withStyles(styles)(DrawerRight);
