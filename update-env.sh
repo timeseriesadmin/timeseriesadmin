f5f1c1df7634f62a427c89c7f54a7b45189862c4
Jan Grzegorowski <grzegorowski@gmail.com>
2018-10-09 08:50:13 +0200

    chore: adjust package.json scripts

--- update-env.sh
+++ update-env.sh
@@ -5,19 +5,6 @@ LATEST_TAG=$(git describe --abbrev=0 --tags)
 echo "Current version is $LATEST_TAG"
 echo "Updating package.json version"
 
-# -E/-rsee: http://www.grymoire.com/Unix/Sed.html#uh-4a
-if [[ $OS == "Darwin" ]]; then
-  # -i see: http://www.grymoire.com/Unix/Sed.html#uh-62h
-  # edit "in place": -i
-  # replace only first occurence: 1,
-  # when line has string: version
-  # of: [0-9]+\.[0-9]+\.[0-9]+
-  # with: LATEST_TAG
-  sed -E -i '' "1,/version/s/[0-9]+\.[0-9]+\.[0-9]+/${LATEST_TAG}/" package.json
-else
-  sed -r -i "1,/version/s/[0-9]+\.[0-9]+\.[0-9]+/${LATEST_TAG}/" package.json
-fi
-
 echo "Updating .env file"
 
 if [[ $OS == "Darwin" ]]; then
