8295446194f9c759c02e9b45fd95ba1aa86736c3
Jan Grzegorowski <grzegorowski@gmail.com>
2018-10-09 22:26:55 +0200

    chore: update version in .env and update-env.sh script

--- update-env.sh
+++ update-env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 OS=$(uname)
 
-LATEST_TAG=$(git describe --abbrev=0 --tags)
-echo "Current version is $LATEST_TAG"
-echo "Updating package.json version"
+# LATEST_TAG=$(git describe --abbrev=0 --tags)
+VERSION=$(grep '"version":' package.json | cut -d\" -f4)
+echo "Current version is $VERSION"
 
 echo "Updating .env file"
 
 if [[ $OS == "Darwin" ]]; then
-  sed -E -i '' "1,/REACT_APP_VERSION=/s/REACT_APP_VERSION=.*/REACT_APP_VERSION=${LATEST_TAG}/" .env
+  sed -E -i '' "1,/REACT_APP_VERSION=/s/REACT_APP_VERSION=.*/REACT_APP_VERSION=${VERSION}/" .env
 else
-  sed -r -i "1,/REACT_APP_VERSION=/s/REACT_APP_VERSION=.*/REACT_APP_VERSION=${LATEST_TAG}/" .env
+  sed -r -i "1,/REACT_APP_VERSION=/s/REACT_APP_VERSION=.*/REACT_APP_VERSION=${VERSION}/" .env
 fi
