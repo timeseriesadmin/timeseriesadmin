e4683b3c4da0eed03e20aa5907fbd10fe937704d
Jan Grzegorowski <grzegorowski@gmail.com>
2018-12-11 22:41:33 +0100

    refactor: TableToolbar, extract styles file

--- src/components/ResultsTable/TableToolbar/index.jsx
+++ src/components/ResultsTable/TableToolbar/index.jsx
@@ -1,63 +1,61 @@
 // @flow
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { FormGroup, Toolbar, Typography, FormLabel, Select, MenuItem } from '@material-ui/core';
+import {
+  FormGroup,
+  Toolbar,
+  Typography,
+  FormLabel,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
+import styles from './style';
 
-const styles = theme => ({
-  root: {
-    paddingRight: theme.spacing.unit,
-  },
-  spacer: {
-    // flex: '1 1 100%',
-  },
-  actions: {
-    minWidth: 320,
-    // flex: '0 0 auto',
-    color: theme.palette.text.secondary,
-  },
-  title: {
-    paddingTop: 18,
-    flexGrow: 1,
-    // flex: '0 0 auto',
-  },
-});
 type Props = {
   title: string,
   classes: any,
-	hasTime: boolean,
-	timeFormat: 's'|'ms'|'ns'|'timestamp',
-	handleFormatChange: Function,
+  hasTime: boolean,
+  timeFormat: 's' | 'ms' | 'ns' | 'timestamp',
+  handleFormatChange: Event => void,
 };
-const TableToolbar = ({ classes, title, timeFormat, handleFormatChange, hasTime }: Props) => (
-<Toolbar className={classes.root}>
-  <div className={classes.title}>
-    <Typography variant="subheading" id="tableTitle">
-      {title}
-    </Typography>
-  </div>
-  <div className={classes.spacer} />
-	{hasTime &&
-  <div className={classes.actions}>
-		<FormGroup row style={{ flexWrap: 'nowrap', display: 'flex' }}>
-			<FormLabel style={{ margin: 8, marginRight: 16 }}>Time format</FormLabel>
-			<Select
-				value={timeFormat}
-				onChange={handleFormatChange}
-				displayEmpty
-				name="timeFormat"
-        style={{ flexGrow: 1 }}
-			>
-				<MenuItem value="timestamp">
-					<em>Timestamp</em>
-				</MenuItem>
-				<MenuItem value="s">Date with seconds</MenuItem>
-				<MenuItem value="ms">Date with milliseconds</MenuItem>
-				<MenuItem value="ns">Date with nanoseconds</MenuItem>
-			</Select>
-		</FormGroup>
-  </div>
-	}
-</Toolbar>
+const TableToolbar = ({
+  classes,
+  title,
+  timeFormat,
+  handleFormatChange,
+  hasTime,
+}: Props) => (
+  <Toolbar className={classes.root}>
+    <div className={classes.title}>
+      <Typography variant="subheading" id="tableTitle">
+        {title}
+      </Typography>
+    </div>
+    <div className={classes.spacer} />
+    {hasTime && (
+      <div className={classes.actions}>
+        <FormGroup row style={{ flexWrap: 'nowrap', display: 'flex' }}>
+          <FormLabel style={{ margin: 8, marginRight: 16 }}>
+            Time format
+          </FormLabel>
+          <Select
+            value={timeFormat}
+            onChange={handleFormatChange}
+            displayEmpty
+            name="timeFormat"
+            style={{ flexGrow: 1 }}
+          >
+            <MenuItem value="timestamp">
+              <em>Timestamp</em>
+            </MenuItem>
+            <MenuItem value="s">Date with seconds</MenuItem>
+            <MenuItem value="ms">Date with milliseconds</MenuItem>
+            <MenuItem value="ns">Date with nanoseconds</MenuItem>
+          </Select>
+        </FormGroup>
+      </div>
+    )}
+  </Toolbar>
 );
 
 export default withStyles(styles)(TableToolbar);
---
+++ src/components/ResultsTable/TableToolbar/style.js
@@ -0,0 +1,21 @@
+// @flow
+const styles = (theme: any) => ({
+  root: {
+    paddingRight: theme.spacing.unit,
+  },
+  spacer: {
+    // flex: '1 1 100%',
+  },
+  actions: {
+    minWidth: 320,
+    // flex: '0 0 auto',
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    paddingTop: 18,
+    flexGrow: 1,
+    // flex: '0 0 auto',
+  },
+});
+
+export default styles;
