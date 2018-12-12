d834af2ed60ad4b4b4feba6a5de301f07e65abf4
Jan Grzegorowski <grzegorowski@gmail.com>
2018-12-12 22:29:18 +0100

    feat: add labels to input fields

--- src/components/FormInflux/index.jsx
+++ src/components/FormInflux/index.jsx
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 import { Query, Mutation } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
 import { Button, Grid } from '@material-ui/core';
-import { Form, Field } from 'react-final-form'
+import { Form, Field } from 'react-final-form';
 import get from 'lodash/get';
 
 import { composeValidators, isRequired } from '../../helpers/validators';
@@ -36,114 +36,133 @@ const FormInflux = (props: Props) => {
   const { classes, onSubmit } = props;
 
   return (
-    <Query query={GET_INITIAL} >
+    <Query query={GET_INITIAL}>
       {({ loading, error, data }) => {
-      const initialValues = get(data, 'form', {});
-      return (
-        <Form onSubmit={onSubmit}
-          initialValues={initialValues}
-          render={({ handleSubmit, form, submitting, pristine, values }) => (
-            <form onSubmit={handleSubmit} className={classes.form}>
-              {/* It is here to prevent Chrome from autofilling user and password form fields */}
-              <input type="password" style={{ display: 'none' }} />
+        const initialValues = get(data, 'form', {});
+        return (
+          <Form
+            onSubmit={onSubmit}
+            initialValues={initialValues}
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+              <form onSubmit={handleSubmit} className={classes.form}>
+                {/* It is here to prevent Chrome from autofilling user and password form fields */}
+                <input type="password" style={{ display: 'none' }} />
 
-              <Grid container spacing={16}>
+                <Grid container spacing={16}>
+                  <Grid item xs={6}>
+                    <Field
+                      id="influx-url"
+                      disabled={submitting || loading}
+                      name="url"
+                      component={renderField}
+                      label="Database URL"
+                      placeholder="https://myinfluxdb.test:8086"
+                      validate={composeValidators(isRequired)}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Field
+                      id="influx-u"
+                      disabled={submitting || loading}
+                      name="u"
+                      component={renderField}
+                      label="User"
+                    />
+                  </Grid>
 
-                <Grid item xs={6}>
-                  <Field
-                    disabled={submitting || loading}
-                    name="url"
-                    component={renderField}
-                    label="Database URL"
-                    placeholder="https://myinfluxdb.test:8086"
-                    validate={composeValidators(isRequired)}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <Field
-                    disabled={submitting || loading}
-                    name="u"
-                    component={renderField}
-                    label="User"
-                  />
-                </Grid>
+                  <Grid item xs={6}>
+                    <Field
+                      id="influx-p"
+                      disabled={submitting || loading}
+                      name="p"
+                      component={renderField}
+                      label="Password"
+                      type="password"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Field
+                      id="influx-db"
+                      disabled={submitting || loading}
+                      name="db"
+                      component={renderField}
+                      label="Database"
+                    />
+                  </Grid>
 
-                <Grid item xs={6}>
-                  <Field
-                    disabled={submitting || loading}
-                    name="p"
-                    component={renderField}
-                    label="Password"
-                    type="password"
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <Field
-                    disabled={submitting || loading}
-                    name="db"
-                    component={renderField}
-                    label="Database"
-                  />
-                </Grid>
+                  <Grid item xs={12}>
+                    <Mutation mutation={SAVE_CONNECTION} variables={values}>
+                      {(mutate, { loading, error }) => (
+                        <Button
+                          disabled={loading}
+                          type="button"
+                          variant="outlined"
+                          color="primary"
+                          style={{ float: 'right' }}
+                          onClick={() => mutate()}
+                        >
+                          {loading ? 'Saving...' : 'Save connection data'}
+                        </Button>
+                      )}
+                    </Mutation>
+                  </Grid>
 
-                <Grid item xs={12}>
-                  <Mutation mutation={SAVE_CONNECTION} variables={values}>
-                    {(mutate, { loading, error }) => (
+                  <Grid item xs={12}>
+                    <Field
+                      id="influx-q"
+                      disabled={submitting || loading}
+                      name="q"
+                      component={renderField}
+                      label="Query"
+                      validate={composeValidators(isRequired)}
+                      multiline
+                      rows={5}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12} className={classes.footer}>
                     <Button
-                      disabled={loading}
-                      type="button"
-                      variant="outlined"
-                      color="primary"
-                      style={{ float: 'right' }}
-                      onClick={() => mutate()}
+                      disabled={submitting || loading}
+                      type="submit"
+                      variant="contained"
+                      color="secondary"
+                      className={classes.submit}
+                      classes={{
+                        root: classes.submit,
+                        disabled: classes.disabled,
+                      }}
                     >
-                      {loading ? 'Saving...' : 'Save connection data'}
+                      {submitting
+                        ? 'Executing query...'
+                        : loading
+                          ? 'Loading saved'
+                          : 'Run query'}
                     </Button>
-                    )}
-                  </Mutation>
+                  </Grid>
                 </Grid>
-
-                <Grid item xs={12}>
-                  <Field
-                    disabled={submitting || loading}
-                    name="q"
-                    component={renderField}
-                    label="Query"
-                    validate={composeValidators(isRequired)}
-                    multiline
-                    rows={5}
-                  />
-                </Grid>
-
-                <Grid item xs={12} className={classes.footer}>
-                  <Button
-                    disabled={submitting || loading}
-                    type="submit"
-                    variant="contained"
-                    color="secondary"
-                    className={classes.submit}
-                    classes={{root: classes.submit, disabled: classes.disabled}}
-                  >
-                    {submitting ? 'Executing query...' : loading ? 'Loading saved' : 'Run query'}
-                  </Button>
-                </Grid>
-
-              </Grid>
-            </form>
-          )}
-        />
-      );
+              </form>
+            )}
+          />
+        );
       }}
     </Query>
   );
 };
 
-const GET_INITIAL = gql`{
-  form @client { url u p db q }
-}`;
+const GET_INITIAL = gql`
+  {
+    form @client {
+      url
+      u
+      p
+      db
+      q
+    }
+  }
+`;
 
 const SAVE_CONNECTION = gql`
-  mutation ($url: String!, $u: String, $p: String, $db: String) {
+  mutation($url: String!, $u: String, $p: String, $db: String) {
     saveConnection(url: $url, u: $u, p: $p, db: $db) @client
   }
 `;
--- src/helpers/form.jsx
+++ src/helpers/form.jsx
@@ -1,7 +1,10 @@
 // @flow
 import React from 'react';
 import {
-  InputLabel, FormControl, FormHelperText, TextField,
+  InputLabel,
+  FormControl,
+  FormHelperText,
+  TextField,
 } from '@material-ui/core';
 import PasswordField from 'material-ui-password-field';
 
@@ -10,32 +13,24 @@ export const renderField = ({
   label,
   meta: { touched, error },
   ...custom
-}: any): any => (custom.type === 'password' ? (
-  <FormControl error={touched && !!error} fullWidth>
-    <InputLabel htmlFor={custom.name}>Password</InputLabel>
-    <PasswordField
-      id={custom.name}
-      {...input}
-      {...custom}
-    />
-    {touched && !!error ? (
-      <FormHelperText>{error}</FormHelperText>
-    ) : ''}
-  </FormControl>
-
-) : (
-  <FormControl error={touched && !!error} fullWidth>
-    <TextField
-      error={touched && !!error}
-      label={label}
-      fullWidth
-      {...input}
-      {...custom}
-    />
-    {touched && !!error ? (
-      <FormHelperText>{error}</FormHelperText>
-    ) : ''}
-  </FormControl>
-));
+}: any): any =>
+  custom.type === 'password' ? (
+    <FormControl error={touched && !!error} fullWidth>
+      <InputLabel htmlFor={custom.id}>Password</InputLabel>
+      <PasswordField {...input} {...custom} />
+      {touched && !!error ? <FormHelperText>{error}</FormHelperText> : ''}
+    </FormControl>
+  ) : (
+    <FormControl error={touched && !!error} fullWidth>
+      <TextField
+        error={touched && !!error}
+        label={label}
+        fullWidth
+        {...input}
+        {...custom}
+      />
+      {touched && !!error ? <FormHelperText>{error}</FormHelperText> : ''}
+    </FormControl>
+  );
 
 export default { renderField };
