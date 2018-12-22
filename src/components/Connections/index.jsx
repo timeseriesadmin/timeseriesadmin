8af99161504e99c10b2710e594f670bea35a74ef
Jan Grzegorowski <grzegorowski@gmail.com>
2018-12-22 13:19:06 +0100

    test: add Connections component basic test

--- src/components/Connections/index.jsx
+++ src/components/Connections/index.jsx
@@ -30,18 +30,26 @@ const styles = theme => ({
   },
 });
 
-const SET_FORM_QUERY = gql`
-  mutation ($url: String!, $u: String, $p: String, $db: String) {
+export const SET_FORM_QUERY = gql`
+  mutation($url: String!, $u: String, $p: String, $db: String) {
     updateForm(url: $url, u: $u, p: $p, db: $db) @client
   }
 `;
 
-const GET_CONNECTIONS = gql`{
-  connections @client { id url u p db }
-}`;
+export const GET_CONNECTIONS = gql`
+  {
+    connections @client {
+      id
+      url
+      u
+      p
+      db
+    }
+  }
+`;
 
-const DELETE_CONNECTION = gql`
-  mutation ($id: String!) {
+export const DELETE_CONNECTION = gql`
+  mutation($id: String!) {
     deleteConnection(id: $id) @client
   }
 `;
@@ -51,52 +59,67 @@ type Props = {
 };
 const Connections = ({ classes }: Props) => (
   <Query query={GET_CONNECTIONS}>
-  {({ loading, error, data }: QueryRenderProps<{ connections: any[] }>) => (
-    <Mutation mutation={DELETE_CONNECTION}>
-    {deleteConnection => {
-      const handleDelete = (id) => () => {
-        deleteConnection({ variables: { id } });
-      };
-      return (
-        <Mutation mutation={SET_FORM_QUERY}>
-        {updateForm =>
-          loading ?  <div>Loading...</div> 
-          : !data || !data.connections || data.connections.length === 0 ?  <div>No data</div>
-          : error ? <div>Error!</div>
-          : <List className={classes.list}>
-            {data.connections.map((conn, index) => (
-              <ListItem key={index} className={classes.listItem}>
-                <Button className={classes.btnConnect}
-                  onClick={() => updateForm({ variables: { ...conn } })}
-                >
-                  <Typography variant="body1">
-                    {conn.url}
-                  </Typography>
-                  {conn.db &&
-                  <Typography variant="body1" color="textSecondary" style={{ fontSize: 12 }}>
-                    database: {conn.db}
-                  </Typography>
-                  }
-                  {conn.u &&
-                  <Typography variant="body1" color="textSecondary" style={{ fontSize: 12 }}>
-                    user: {conn.u}
-                  </Typography>
-                  }
-                </Button>
-                <Button className={classes.btnDelete}
-                  onClick={handleDelete(conn.id)}
-                >
-                  <DeleteIcon style={{ fontSize: 18 }}/>
-                </Button>
-              </ListItem>
-            ))}
-          </List>
-        }
-        </Mutation>
-      );
-    }}
-    </Mutation>
-  )}
+    {({ loading, error, data }: QueryRenderProps<{ connections: any[] }>) => (
+      <Mutation mutation={DELETE_CONNECTION}>
+        {deleteConnection => {
+          const handleDelete = id => () => {
+            deleteConnection({ variables: { id } });
+          };
+          return (
+            <Mutation mutation={SET_FORM_QUERY}>
+              {updateForm =>
+                loading ? (
+                  <div>Loading...</div>
+                ) : !data ||
+                !data.connections ||
+                data.connections.length === 0 ? (
+                  <div>No data</div>
+                ) : error ? (
+                  <div>Error!</div>
+                ) : (
+                  <List className={classes.list}>
+                    {data.connections.map((conn, index) => (
+                      <ListItem key={index} className={classes.listItem}>
+                        <Button
+                          className={classes.btnConnect}
+                          onClick={() => updateForm({ variables: { ...conn } })}
+                        >
+                          <Typography variant="body1">{conn.url}</Typography>
+                          {conn.db && (
+                            <Typography
+                              variant="body1"
+                              color="textSecondary"
+                              style={{ fontSize: 12 }}
+                            >
+                              database: {conn.db}
+                            </Typography>
+                          )}
+                          {conn.u && (
+                            <Typography
+                              variant="body1"
+                              color="textSecondary"
+                              style={{ fontSize: 12 }}
+                            >
+                              user: {conn.u}
+                            </Typography>
+                          )}
+                        </Button>
+                        <Button
+                          className={classes.btnDelete}
+                          onClick={handleDelete(conn.id)}
+                        >
+                          <DeleteIcon style={{ fontSize: 18 }} />
+                        </Button>
+                      </ListItem>
+                    ))}
+                  </List>
+                )
+              }
+            </Mutation>
+          );
+        }}
+      </Mutation>
+    )}
   </Query>
 );
 
---
+++ src/components/Connections/index.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, waitForElement } from 'test-utils';
+import Connections, { GET_CONNECTIONS } from './index';
+
+const mocks = [
+  {
+    request: {
+      query: GET_CONNECTIONS,
+    },
+    result: {
+      data: {
+        connections: [
+          // { id: 'test', url: 'test', u: 'user', p: 'pass', db: 'db' },
+        ],
+      },
+    },
+  },
+];
+
+describe('<Connections />', () => {
+  test('rendering', async () => {
+    const { getByText } = render(<Connections mocks={mocks} />);
+
+    expect(getByText('Loading...')).toBeDefined();
+    await waitForElement(() => getByText('No data'));
+  });
+});
