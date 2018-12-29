632265cb39c0248496f0ef54be694082f7100ede
Jan Grzegorowski <grzegorowski@gmail.com>
2018-12-29 23:12:25 +0100

    test: provide better ApolloClient mock

--- utils/test-utils.js
+++ utils/test-utils.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { MockedProvider } from 'react-apollo/test-utils';
+import { ApolloProvider } from 'react-apollo';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { withClientState } from 'apollo-link-state';
 
 const customRender = (node, options) => {
   const rendered = render(
-    node.props && node.props.mocks ? (
+    node.props && node.props.client ? (
+      <ApolloProvider client={node.props.client}>{node}</ApolloProvider>
+    ) : node.props && node.props.mocks ? (
       <MockedProvider mocks={node.props.mocks} addTypename={false}>
         {node}
       </MockedProvider>
@@ -18,6 +26,26 @@ const customRender = (node, options) => {
     rerender: (ui, options) =>
       customRender(ui, { container: rendered.container, ...options }),
   };
+};
+
+// custom mocked apollo client
+export const setupClient = resolvers => {
+  const cache = new InMemoryCache();
+
+  const linkState = withClientState({
+    resolvers,
+    defaults: {},
+    cache,
+    typeDefs: '',
+  });
+
+  const client = new ApolloClient({
+    cache,
+    // mocks are provided as link state resolvers
+    link: ApolloLink.from([linkState]),
+  });
+
+  return client;
 };
 
 // re-export everything
