cd3289f032a2b822718d8d8ab28c2604c7b33c74
Jan Grzegorowski <grzegorowski@gmail.com>
2018-12-31 15:34:55 +0100

    feat: improve PanelExplorer connection info

--- src/components/PanelExplorer/index.jsx
+++ src/components/PanelExplorer/index.jsx
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Explorer from '../Explorer';
@@ -7,11 +9,17 @@ import Explorer from '../Explorer';
 const styles = theme => ({
   root: {
     paddingTop: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit*2,
+    paddingLeft: theme.spacing.unit * 2,
   },
   info: {
-    padding: theme.spacing.unit*2,
+    padding: theme.spacing.unit * 2,
     paddingBottom: 0,
+  },
+  noList: {
+    paddingTop: 8,
+    paddingLeft: 0,
+    paddingRight: 16,
+    fontSize: 14,
   },
 });
 
@@ -19,14 +27,36 @@ type Props = {
   classes: any,
 };
 const PanelExplorer = ({ classes }: Props) => (
-  <div>
-    <Typography variant="body1" className={classes.info}>
-      Explore currently connected server
-    </Typography>
-    <div className={classes.root}>
-      <Explorer/>
-  </div>
-  </div>
+  <Query query={GET_SERVER}>
+    {({ data }) => (
+      <div>
+        <Typography variant="body1" className={classes.info}>
+          {data && data.form && data.form.url
+            ? `Connected to ${data.form.url}`
+            : 'Not connected'}
+        </Typography>
+        <div className={classes.root}>
+          {data && data.form && data.form.url ? (
+            <Explorer />
+          ) : (
+            <div className={classes.noList}>
+              Use "RUN QUERY" button to connect to a server,
+              <br />
+              or select one from "CONNECT" panel.
+            </div>
+          )}
+        </div>
+      </div>
+    )}
+  </Query>
 );
+
+const GET_SERVER = gql`
+  {
+    form @client {
+      url
+    }
+  }
+`;
 
 export default withStyles(styles)(PanelExplorer);
