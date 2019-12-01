2488cd6422caf4a0a5c7a0a869d0ec916ee8805f
Jan Grzegorowski <grzegorowski@gmail.com>
2019-12-01 20:28:19 +0100

    Fix window.navigator tests

--- src/apollo/helpers/isElectron.test.ts
+++ src/apollo/helpers/isElectron.test.ts
@@ -1,21 +1,21 @@
 import { isElectron } from './isElectron';
 
 test('isElectron()', () => {
-  const windowSpy = jest.spyOn(global as any, 'window', 'get');
-  windowSpy.mockImplementation(() => ({
-    navigator: {
-      userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) timeseriesadmin/0.1.4 Chrome/78.0.3904.113 Electron/7.1.2 Safari/537.36',
-    },
-  }));
+  const navigator = (global as any).window.navigator;
+  delete (global as any).window.navigator;
+  (global as any).window.navigator = {
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) timeseriesadmin/0.1.4 Chrome/78.0.3904.113 Electron/7.1.2 Safari/537.36',
+  };
+
   expect(isElectron()).toBe(true);
 
-  windowSpy.mockImplementation(() => ({
-    navigator: {
-      userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-    },
-  }));
+  delete (global as any).window.navigator;
+  (global as any).window.navigator = {
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+  };
   expect(isElectron()).toBe(false);
-  windowSpy.mockRestore();
+
+  (global as any).window.navigator = navigator;
 });
