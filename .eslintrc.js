54921fe476484b8b661ef6346986a8c6c848a9ce
Jan Grzegorowski <grzegorowski@gmail.com>
2019-12-06 00:23:14 +0100

    Adjust Eslint configuration for Typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
-  env: {
-    'cypress/globals': true,
-    browser: true,
-    jasmine: true,
-    jest: true,
-  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'react-app',
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
-  plugins: ['cypress', 'react', '@typescript-eslint', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    jsx: true,
+  },
 };
