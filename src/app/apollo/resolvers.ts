6dd457a6bd4f05fa0e99c355fe5419ff6564347f
Jan Grzegorowski <grzegorowski@gmail.com>
2019-12-16 19:28:09 +0100

    Remove unused code

--- src/app/apollo/resolvers.ts
+++ src/app/apollo/resolvers.ts
@@ -2,7 +2,6 @@ import { saveConnection, deleteConnection } from './resolvers/connections';
 import { setOpenDrawer, setDrawerWidth } from './resolvers/drawer';
 import { updateForm } from './resolvers/form';
 import { executeQuery } from './resolvers/executeQuery';
-import { setResultsTable } from './resolvers/results';
 
 export const resolvers = {
   Mutation: {
@@ -12,6 +11,5 @@ export const resolvers = {
     setDrawerWidth,
     updateForm,
     executeQuery,
-    setResultsTable,
   },
 };
