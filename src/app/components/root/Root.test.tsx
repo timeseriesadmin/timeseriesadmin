b1eb7b0623bbb26a5a2fa9b51c71092fddeaba3a
Jan Grzegorowski <grzegorowski@gmail.com>
2019-12-18 00:43:25 +0100

    Add form tests

--- src/app/components/root/Root.test.tsx
+++ src/app/components/root/Root.test.tsx
@@ -4,16 +4,15 @@ import { Root } from './Root';
 import { fireEvent, within } from '@testing-library/react';
 
 describe('<App />', () => {
-  test('rendering empty data', async () => {
+  test('connection and query history saving', async () => {
     const { getByText, getByLabelText, getByTestId } = render(<Root />);
-
     const sidebar = within(getByTestId('DrawerRight'));
-    expect(getByText('Time Series Admin')).toBeDefined();
-    expect(getByLabelText('Close sidebar')).toBeDefined();
 
+    // given opened connections
     fireEvent.click(sidebar.getByText('Connect'));
     expect(getByText(/No saved connections./)).toBeDefined();
 
+    // when fill data
     fireEvent.change(getByLabelText('Database URL'), {
       target: { value: 'http://test.test:8086' },
     });
@@ -29,11 +28,23 @@ describe('<App />', () => {
     fireEvent.change(getByLabelText('Query'), {
       target: { value: 'test query' },
     });
+
     fireEvent.click(getByText('Save connection data'));
+
+    // then
     await waitForElement(() => getByText('List of all saved connections'));
 
     expect(sidebar.getByText('http://test.test:8086')).toBeDefined();
     expect(sidebar.getByText('database: test_db')).toBeDefined();
     expect(sidebar.getByText('user: test_user')).toBeDefined();
+
+    // given form submit
+    fireEvent.submit(getByText('Run query'));
+
+    // when on history
+    fireEvent.click(sidebar.getByText('History'));
+
+    // then
+    await waitForElement(() => sidebar.getByText('test query'));
   });
 });
